30ab0abdcb3a36ecc8fd58bfaa96a79d051fa168
Ruslan Vostretsov <ElectricalBrain@protonmail.com>
2023-07-23 13:40:16 +0300

    Unordered set draft

---
+++ c++/include/uset_interface.hpp
@@ -0,0 +1,25 @@
+#ifndef USET_INTERFACE_H_
+#define USET_INTERFACE_H_
+
+#include <cstddef>
+
+
+template<typename T>
+class UnsortedSetI {
+    public:
+        explicit UnsortedSetI() : _length(0) { }
+        virtual ~UnsortedSetI() override = 0;
+
+        inline size_t length () const       { return (_length); }
+
+        inline virtual bool insert (const T& elem) = 0;
+        inline virtual bool remove (const T& i) = 0;
+
+        inline virtual bool find (const T& elem) = 0;
+
+    protected:
+        size_t _length;
+};
+
+
+#endif // USET_INTERFACE_H_
