8ac0fca37f76c10d33e3242954a968c1ef5d6452
Ruslan Vostretsov <ElectricalBrain@protonmail.com>
2023-07-23 21:49:07 +0300

    Improve circular buffer

--- c++/include/circular_buffer.hpp
+++ c++/include/circular_buffer.hpp
@@ -1,20 +1,24 @@
 #pragma once
 
-//
+#include "queue.hpp"
 
 
 template<typename T, size_t maxLength_ = 0>
-class CircularBuffer {
+class CircularBuffer : ContainerInterface<T> {
     public:
-        //
+        virtual ~CircularBuffer() override { clear(); }
 
         inline bool maxLength () const noexcept { return (maxLength_); }
         inline bool isFull () const noexcept {
             return (this->length() == maxLength());
         }
 
+        bool get (T& dest);
+        bool put (const T& val);
+
     protected:
-        // constexpr size_t maxLength_{0};
+        // size_t maxLength_{0};
         // size_t head_{0}, tail_{0};
+        Queue<T> queue_;
         bool overwritable_ { false };
 };
