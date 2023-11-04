9300ccbb67212ed10c362dbcb4d0a453b53b5f4f
Ruslan Vostretsov <ElectricalBrain@protonmail.com>
2023-11-04 19:16:44 +0300

    Add binary search draft

--- c++/include/algorithms/binary_search.hpp
+++ c++/include/algorithms/binary_search.hpp
@@ -4,6 +4,38 @@
 // #include "list_interface.hpp"
 
 
+template<typename T, typename ArrayType>
+bool binarySearch (ArrayType<T>& array, T& target, size_t& targetPosition) {
+    bool targetFound(false);
+    size_t startPosition(0);
+    auto endPosition(array.size() - 1);
+    auto searchPosition = (endPosition - startPosition) / 2 + startPosition;
+
+    while (startPosition <= endPosition) {
+        //auto searchPosition = (endPosition - startPosition) / 2 + startPosition;
+        if (target < array[searchPosition]) {
+            endPosition = searchPosition - 1;
+        }
+        else if (target > array[searchPosition]) {
+            startPosition = searchPosition + 1;
+        }
+        else [[unlikely]] {
+            break;
+        }
+        searchPosition = (endPosition - startPosition) / 2 + startPosition;
+    }
+    // if (array[startPosition] == target) {
+    //     targetPosition = startPosition;
+    //     targetFound = true;
+    // }
+    if (array[searchPosition] == target) {
+        targetPosition = searchPosition;
+        targetFound = true;
+    }
+
+    return targetFound;
+}
+
 // template<typename T>
 // size_t binarySearch (ListInterface<T> list, T& val) {
 //     size_t n = list.length() / 2;
